171c874c639ea6cc3bb5a07cc912c671ac276058
Tyler Balaskovitz <tylerbalaskovitz@yahoo.com>
2024-10-10 20:32:38 -0400

    Learning how to take a tuple and make it into a json-- convert it to a list and then use json.dumps() to serialize it

--- tuples.py
+++ tuples.py
@@ -17,7 +17,7 @@ print(my_tuple[0])  # Output: 1
 print("this is tuple my_tuple[3]")
 print(my_tuple[3])  # Output: 'apple'
 
-local_directory = 'home/Tbone/PYTHONTRASHME'
+local_directory = '/home/Tbone/PYTHONTRASHME'
 
 if not os.path.exists(local_directory):
 	os.makedirs(local_directory)
@@ -25,6 +25,13 @@ if not os.path.exists(local_directory):
 if os.path.exists(local_directory):
 	os.rmdir(local_directory)
 	logger.error("Directory succesfully removed")
+
 single_tuple = (5,)
+my_tuple_to_list = list(my_tuple)
+json_data = json.dumps(my_tuple_to_list)
+print("json data below:")
+print(json_data)
+
 logger.info(my_tuple)
+print("Single Tuple here:")
 print(single_tuple)
