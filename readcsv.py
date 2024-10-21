9f526688c3c89ec6a3ca324cd2fb47ec4a711eac
Tyler Balaskovitz <tylerbalaskovitz@yahoo.com>
2024-10-20 22:47:43 -0400

    practiced transforming the data from one type to another and exporting that as a CSV

--- readcsv.py
+++ readcsv.py
@@ -1,4 +1,9 @@
-import pandas as pd
+import pandas as pd 
 
 df = pd.read_csv("output.csv")
+print(df)
+df['Bonus'] = df['Salary'] * 0.10
+average_salary= df['Salary'].mean()
+average_df = pd.DataFrame({'Average Salary': [average_salary]})
+average_df.to_csv("average_salary",index=False)
 print(df)
