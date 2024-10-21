80d14fa100696c9119ead8e6c63bdc56e99197ee
Tyler Balaskovitz <tylerbalaskovitz@yahoo.com>
2024-10-20 22:49:43 -0400

    Also saw how you can add columns with pandas and then export that to a csv as well

--- readcsv.py
+++ readcsv.py
@@ -6,4 +6,5 @@ df['Bonus'] = df['Salary'] * 0.10
 average_salary= df['Salary'].mean()
 average_df = pd.DataFrame({'Average Salary': [average_salary]})
 average_df.to_csv("average_salary",index=False)
+df.to_csv("bonus_included", index=False)
 print(df)
